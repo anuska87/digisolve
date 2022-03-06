cddba323552d0ffbad1459bfd56c81f8d148c02a
anuska87 <nitta_ea_87@hotmail.com>
2022-03-06 14:49:46 +0100

    Add fade animation to about section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,26 @@
+
 $(document).ready(function() {
-  /** Example JQuery */
-  // $(".services-section").click(function() {
-  //   $(this).css("background-color", "black");
-  // });
+  /** BASIC EXAMPLE JQuery */
+  // $(document).ready(function() {
+  //   $(".services-section").click(function() {
+  //     $(this).css("background-color", "black");
+  //   });
+  // })
 
   /** Waypoint from: http://imakewebthings.com/waypoints/ */
+  /** Adding Sticky navigation */
   $(".about-section").waypoint(function(direction) {
     if(direction === "down"){
       $("nav").addClass('sticky-nav');
     } else {
       $("nav").removeClass('sticky-nav');
     }
+  });
+
+  /** Animation on scroll */
+  $(".js--about-section").waypoint(function() {
+    $(".js--about-box").addClass("animate__animated animate__fadeIn");
+  }, {
+    offset: "50%"
   });
 });
